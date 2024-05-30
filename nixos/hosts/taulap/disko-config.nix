e77052d7338053f2e4db0dd305d3b065db4e6d97
givtrah <givtrah@givtrah.org>
2024-05-30 22:00:59 +0200

    laptop added

---
+++ nixos/hosts/taulap/disko-config.nix
@@ -0,0 +1,64 @@
+{
+  disko.devices = {
+    disk.nvme = {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "1024M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "defaults" ];
+            };
+          };
+          swap = {
+            size = "16G";
+            content = {
+              type = "swap";
+              };
+            };
+          root = {
+            size = "200G";
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ]; 
+              subvolumes = {
+                "/rootfs" = {
+                  mountpoint = "/";
+                  mountOptions = [ "compress=lzo" "noatime" ];
+                  };
+                };
+              };
+            };
+           luks = {
+             size = "100%";
+             content = {
+               type = "luks";
+               name = "crypted";
+               settings = { allowDiscards = true ; };
+               content = {
+                 type = "btrfs";
+                 extraArgs = [ "-f" ];
+                 subvolumes = {
+                   "/home" = {
+                     mountpoint = "/home";
+                     mountOptions = [ "compress=lzo" "noatime" ];
+                     };
+                   "/nix" = {
+                     mountpoint = "/nix";
+                     mountOptions = [ "compress=lzo" "noatime" ];
+                     };
+                   };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  }
