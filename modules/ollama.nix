ea88bcf24908aa42029c23a94dbe7e8621a6032f
givtrah <givtrah@givtrah.org>
2025-05-04 10:48:49 +0200

    ollama

--- modules/ollama.nix
+++ modules/ollama.nix
@@ -1,13 +1,14 @@
 { pkgs, lib, ... }:
 {
 
-services.ollama = {
-  enable = true;
-  acceleration = "rocm";
+  services.ollama = {
+    enable = true;
+    acceleration = "rocm";
     # Optional: preload models, see https://ollama.com/library
-  loadModels = [ "gemma3:12b" "deepseek-r1:14b" "deepcoder:14b" "qwen3:14b"];
-};
+    loadModels = [ "gemma3:12b" "deepseek-r1:14b" "deepcoder:14b" "qwen3:14b"];
+  };
 
+  services.open-webui.enable = true;
 
 
 }
